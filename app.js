b4983982bd5f65264b00c4fb5dc82708ebc06835
root <ricardo199221@gmail.com>
2020-02-12 14:34:38 +0000

    change db connect

--- app.js
+++ app.js
@@ -18,15 +18,7 @@ var flash = require("connect-flash");
 
 
 //mongoose.connect("mongodb://localhost:27017/yelp_camp",{useNewUrlParser:true, useUnifiedTopology: true});
-mongoose.connect("mongodb+srv://Lepracone:Rasta74223mongodb@lepracone-yrv8d.gcp.mongodb.net/test?retryWrites=true&w=majority",{
-	useNewUrlParser:true, 
-	useUnifiedTopology: true,
-	useCreateIndex: true
-}).then(() => {
-	console.log("Connected to DB!");
-}).catch(err => {
-	console.log("ERROR", err.message);
-});
+mongoose.connect(process.env.DATABASEURL);
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
